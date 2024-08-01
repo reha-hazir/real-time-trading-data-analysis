ece44bcd98a0511868f1e46e7e5816a1d8607685
reha-hazir <125329438+reha-hazir@users.noreply.github.com>
2024-08-01 23:42:07 +0200

    support level analysis

--- tradingbot.py
+++ tradingbot.py
@@ -2,13 +2,17 @@ import websocket
 import pandas as pd
 import json
 import logging
+import csv
+import os
 from datetime import datetime
 
 symbol = 'dogeusdt'
 interval = '1m'
 current_date = datetime.today().strftime('%d-%m-%y')
-rsi_window = 2
+rsi_window = 14
 close_prices = []
+high_prices = []
+low_prices = []
 average_gain = 0
 average_loss = 0
 SOCKET = f'wss://stream.binance.com:9443/ws/{symbol}@kline_{interval}'
@@ -19,6 +23,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 running = True
+csv_file = 'trading_data.csv'
 
 def on_open(ws):
     logger.info('WebSocket connected.')
@@ -70,8 +75,25 @@ def calculate_rsi():
 
     return rsi
 
+def calculate_pivot_points(high, low, close):
+    pivot_point = (high + low + close) / 3
+    s1 = 2 * pivot_point - high
+    r1 = 2 * pivot_point - low
+    s2 = pivot_point - (high - low)
+    r2 = pivot_point + (high - low)
+    return pivot_point, s1, r1, s2, r2
+
+def write_to_csv(data):
+    file_exists = os.path.isfile(csv_file)
+    
+    with open(csv_file, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(["Date", "Time", "Open Price", "Close Price", "High Price", "Low Price", "Volume", "Number of Trades", "RSI", "Pivot Point", "S1", "R1", "S2", "R2"])
+        writer.writerow(data)
+
 def on_message(ws, message):
-    global running, close_prices, average_gain, average_loss
+    global running, close_prices, high_prices, low_prices, average_gain, average_loss
 
     if not running:
         return
@@ -84,6 +106,8 @@ def on_message(ws, message):
     current_price = float(kline['c'])
     high_price = float(kline['h'])
     low_price = float(kline['l'])
+    volume = kline['v']
+    number_of_trades = kline['n']
 
     log_message = (
         f" \n -Date: {current_date}"
@@ -101,10 +125,30 @@ def on_message(ws, message):
     if rsi is not None:
         log_message += f" -RSI: {rsi}"
 
-    logger.info(log_message)
-
     if kline_closed:
         close_prices.append(current_price)
+        high_prices.append(high_price)
+        low_prices.append(low_price)
+
+        if len(close_prices) >= 15:
+            high_15m = max(high_prices[-15:])
+            low_15m = min(low_prices[-15:])
+            close_15m = close_prices[-1]
+            pivot_point, s1, r1, s2, r2 = calculate_pivot_points(high_15m, low_15m, close_15m)
+        else:
+            pivot_point, s1, r1, s2, r2 = None, None, None, None, None
+
+        log_message += (
+            f" -Pivot Point: {pivot_point}"
+            f" -Support 1 (S1): {s1}"
+            f" -Resistance 1 (R1): {r1}"
+            f" -Support 2 (S2): {s2}"
+            f" -Resistance 2 (R2): {r2}"
+        )
+
+        write_to_csv([current_date, event_time, open_price, current_price, high_price, low_price, volume, number_of_trades, rsi, pivot_point, s1, r1, s2, r2])
+
+    logger.info(log_message)
 
 def connect_websocket():
     websocket.enableTrace(False)
@@ -114,3 +158,6 @@ def connect_websocket():
                                 on_message=on_message,
                                 on_error=on_error)
     return ws
+
+ws = connect_websocket()
+ws.run_forever()
